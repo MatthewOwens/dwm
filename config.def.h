b44a5963841986cf108e58aefa688883f08f256f
MatthewOwens <matthew@owens.tech>
2020-05-20 17:12:00 +0100

    rice

--- config.def.h
+++ config.def.h
@@ -2,17 +2,19 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 20;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+//static const char *fonts[]          = { "monospace:size=10" };
+//static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "xos4 Terminus-15","monospace:size=10" };
+static const char dmenufont[]       = "xos4 Terminus-15";
+static const char col_gray1[]       = "#1c1c1c";
+static const char col_gray2[]       = "#1c1c1c";
+static const char col_gray3[]       = "#b79e91";
+static const char col_gray4[]       = "#b35050";
+static const char col_cyan[]        = "#1c1c1c";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
